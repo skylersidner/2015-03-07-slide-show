989b667783ae1ecb8d5202c8ac2909414b3498e4
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-08 19:53:31 -0500

    tweaked styling; javascript is working;

--- public/global.js
+++ public/global.js
@@ -1,16 +1,26 @@
 window.onload = function() {
   testClick = document.getElementById("test");
   testClick.addEventListener("click", listFirstSlide);
-  listLink.onclick = function() {return false};
+  testClick.onclick = function() {return false};
 
 }
 
 function listFirstSlide() {
-  var js_req = new HMLHttpRequest;
+  var js_req = new XMLHttpRequest;
   js_req.open("get", "http://localhost:4567/test");
   js_req.send();
   js_req.addEventListener("load", function(){
-    r = (JSON.parse(js_req.response));
-    alert("Here is the slide info - ID: " + r.id + ", Title: " + r.title + ", Body Text: " + r.body_text + ", Slide Order: " + r.slide_order + ".");
+    r = (JSON.parse(js_req.response));  
+  
+    var title_text = document.createTextNode(r.title);
+    document.getElementById("title").appendChild(title_text);
+  
+    var text = document.createTextNode(r.body_text);
+    document.getElementById("slide_text").appendChild(text);
+  
+    var order = document.createTextNode(r.slide_order);
+    document.getElementById("current_slide").appendChild(order);
+  
   }, false);
+  
 }
